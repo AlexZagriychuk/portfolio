18f86239864ead53c5e2106589e8a4e2163cfa6b
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-17 01:31:46 -0700

    A few small chenges in the text; changed the way portfolio__project-info is presented in mobile view

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var showMoreElements = document.getElementsByClassName("project-show-more");
   for (var i = 0; i < showMoreElements.length; i++) {
     showMoreElements[i].addEventListener("click", function (e) {
-      console.log(e.target);
-      var ancestor = findAncestor(e.target, ".portfolio__project-info");
-      console.log(ancestor);
-      ancestor.classList.add('show-full-description');
+      var projectInfo = findAncestorElement(e.target, ".portfolio__project-info");
+      projectInfo.classList.add('show-full-description');
     }, false);
   }
 });
@@ -14,15 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
   var showLessElements = document.getElementsByClassName("project-show-less");
   for (var i = 0; i < showLessElements.length; i++) {
     showLessElements[i].addEventListener("click", function (e) {
-      console.log(e.target);
-      var ancestor = findAncestor(e.target, ".portfolio__project-info");
-      console.log(ancestor);
-      ancestor.classList.remove('show-full-description');
+      var projectInfo = findAncestorElement(e.target, ".portfolio__project-info");
+      projectInfo.classList.remove('show-full-description');
+      //After hiding the full-description, scroll portfolio into view if project-info is not into view
+      if(!isElementInViewport(projectInfo)) {
+        var portfolio = findAncestorElement(projectInfo, ".portfolio__project");
+        portfolio.scrollIntoView();
+      }
     }, false);
   }
 });
 
-function findAncestor(el, sel) {
+function findAncestorElement(el, sel) {
   while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, sel)));
   return el;
 }
+
+function isElementInViewport(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
